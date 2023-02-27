a0baae69cff58fc2ee99e3a671752b51ee724bc4
Rohan Khanderia <rohan.khanderia@gmail.com>
2023-02-27 18:19:07 -0500

    Add a home screen that has login and sign up buttons

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import HomeScreen from './components/HomeScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>
 );
