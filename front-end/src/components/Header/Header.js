620b50683bb9ffdbee84c0c3034c8d7b118c7eda
Mert Bayturk <mib335@nyu.edu>
2023-03-06 09:56:32 -0500

    Implemented home buttons, layout. And CSS fixing.

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -1,23 +1,22 @@
-import './Header.css'
+import "./Header.css";
 //import logo from './logo.svg'
-import { Link } from 'react-router-dom'
-import HamburgerMenu from './HamburgerMenu.svg'
+import { Link } from "react-router-dom";
+// import HamburgerMenu from "./HamburgerMenu.svg";
 /**
  * Our QuizVerse Header!
  */
-const Header = props => {
+const Header = (props) => {
   return (
     <header className=".Header-header">
       <nav className="Header-navbar">
-        <h1>QuizVerse! (In Progress)</h1>
+        <h1>QuizVerse</h1>
         <ul className="nav-links">
-          {/*Will be a pop-up and not a link*/}
-          <li className="nav-item">
-          <Link to="/menu" className="menu"><img src={HamburgerMenu} alt="Hamburger Menu" /></Link>
-          </li>
           {/*Coins and streak will be eventually pulled from backend*/}
           <li className="nav-item">
-            <Link to="/shop">Current Streak: 1<br></br>100 coins</Link>
+            <Link to="/shop">Current Streak: 3</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/shop">Coins: 100</Link>
           </li>
           {/*This will be set to the right eventually with css*/}
           <li className="nav-item">
@@ -27,20 +26,20 @@ const Header = props => {
             <Link to="/my-account">My Account</Link>
           </li>
         </ul>
-          {/*These are filler just to be used for current navigation purposes*/}
-          <li className="nav-item">
-            <Link to="/">Landing</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/home">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/items">Items</Link>
-          </li>
+        {/*These are filler just to be used for current navigation purposes*/}
+        <li className="nav-item">
+          <Link to="/">Landing</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/home">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/items">Items</Link>
+        </li>
       </nav>
     </header>
-  )
-}
+  );
+};
 
 // make this component available to be imported into any other file
-export default Header
+export default Header;
