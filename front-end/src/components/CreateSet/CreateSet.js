30227b9fcd9a4b34f941ace3d4713b3e838f0034
chiyanz <jonathanqyz@gmail.com>
2023-03-06 19:27:36 -0500

    update to create-set page formatting and debug footer interaction

--- front-end/src/components/CreateSet/CreateSet.js
+++ front-end/src/components/CreateSet/CreateSet.js
@@ -9,53 +9,120 @@ const CreateSet = (props) => {
 
 
   return (
-    <Container 
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: 'auto',
-        overflow: "hidden",
-        overflowY: "scroll",
-      }}>
-      <FormControl sx={{ m: 1, width: '30ch'}} variant="outlined">
-        <TextField 
-          margin="dense"
-          id="filled-basic" label="Subject, chapter, unit, etc" variant="filled" size="small" value={title} helperText="TITLE" onChange={(evt) => {
-            setTitle(evt.target.value)
-        }}/>
-        <TextField id="filled-basic" label="Enter a description" helperText="DESCRIPTION" variant="filled" multiline rows={3} value={description} onChange={(evt) => {
-          setDescription(evt.target.value)
-        }}/>
-      </FormControl>
+    <div>
+      <Container
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          overflow: 'auto',
+          overflowY: 'scroll'
+        }}>
+        <FormControl sx={{ m: 1, width: '86vw' }} variant="outlined">
+          <TextField
+            margin="dense"
+            id="filled-basic"
+            label="Subject, chapter, unit, etc"
+            variant="filled"
+            size="small"
+            value={title}
+            helperText="TITLE"
+            onChange={(evt) => {
+              setTitle(evt.target.value);
+            }}
+          />
+          <TextField
+            id="filled-basic"
+            label="Enter a description"
+            helperText="DESCRIPTION"
+            variant="filled"
+            multiline
+            rows={3}
+            value={description}
+            onChange={(evt) => {
+              setDescription(evt.target.value);
+            }}
+          />
+        </FormControl>
 
-      <Box sx={{
-        width: "60vw",
-        height: "auto",
-        margin: "auto",
-        borderRadius: 1,
-      }}>
-        <Stack spacing={1}>
-          <TextField id="filled-basic"  variant="standard" size="small"  helperText="TERM" onChange={(evt) => {
-                
-          }}/>
-          <TextField id="filled-basic"  variant="standard" size="small"  helperText="DEFINITION" onChange={(evt) => {
-                
-          }}/>
-          <TextField id="filled-basic"  variant="standard" size="small"  helperText="TERM" onChange={(evt) => {
-                
-              }}/>
-              <TextField id="filled-basic"  variant="standard" size="small"  helperText="DEFINITION" onChange={(evt) => {
-                    
-              }}/>
-        </Stack>
-        
-      </Box>
-      <Button variant="outlined" startIcon={<FontAwesomeIcon icon={faCirclePlus} />}>
-        Add
-      </Button>
-      <Button variant="outlined">Create</Button>
-    </Container>
-  )
+        <Box
+          sx={{
+            width: '80vw',
+            height: 'auto',
+            margin: 'auto',
+            borderRadius: 1,
+            backgroundColor: 'rgba(0, 7, 111, 0.4)',
+            padding: '10px'
+          }}>
+          <Stack spacing={1}>
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="TERM"
+              onChange={(evt) => {}}
+            />
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="DEFINITION"
+              onChange={(evt) => {}}
+            />
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="TERM"
+              onChange={(evt) => {}}
+            />
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="DEFINITION"
+              onChange={(evt) => {}}
+            />
+          </Stack>
+
+          <Stack spacing={1}>
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="TERM"
+              onChange={(evt) => {}}
+            />
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="DEFINITION"
+              onChange={(evt) => {}}
+            />
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="TERM"
+              onChange={(evt) => {}}
+            />
+            <TextField
+              id="filled-basic"
+              variant="standard"
+              size="small"
+              helperText="DEFINITION"
+              onChange={(evt) => {}}
+            />
+          </Stack>
+        </Box>
+        <Button variant="outlined" startIcon={<FontAwesomeIcon icon={faCirclePlus} />}>
+          Add
+        </Button>
+        <Button variant="outlined">Create</Button>
+      </Container>
+    </div>
+  );
 }
 
 export default CreateSet
