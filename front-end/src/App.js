6f4f5f5bb9b3779e7473fb89a7a85eed79d05031
RohanK6 <rohan.khanderia@gmail.com>
2023-03-09 17:24:32 +0000

    Remove redundant App.js styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Home from './components/Home/Home';
 import Landing from './components/Landing/Landing';
 import Layout from './components/Layout/Layout';
