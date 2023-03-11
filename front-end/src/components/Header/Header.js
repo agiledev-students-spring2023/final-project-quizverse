86588a2bac2ae2ff9c6be5b54c87794639784b29
1019jchen <jyc8938@stern.nyu.edu>
2023-03-11 16:38:56 -0500

    Updated header to include mockaroo

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -9,24 +9,46 @@ import React, { useState, useEffect } from "react"
 import { Navigate, useSearchParams } from "react-router-dom"
 import axios from "axios"
 const Header = (props) => {
+  const [data, setData] = useState([])
 
-    try {
-      // create an object with the data we want to send to the server
-      const requestData = {
-        username: e.target.username.value, // gets the value of the field in the submitted form with name='username'
-        password: e.target.password.value, // gets the value of the field in the submitted form with name='password',
-      }
-      // send the request to the server api to authenticate
-      const response = await axios.post(
-        "https://my.api.mockaroo.com/users.json?key=6b3bc3e0",
-        requestData
-      )
-      // store the response data into the data state variable
-      console.log(response.data)
-    } catch (err) {
-      // throw an error
-      throw new Error(err)
-    }
+  // the following side-effect will be called once upon initial render
+  useEffect(() => {
+    // fetch some mock data about animals for sale
+    console.log("fetching 10 random users...")
+    axios("https://my.api.mockaroo.com/users.json?key=6b3bc3e0")
+      .then(response => {
+        // extract the data from the server response
+        setData(response.data)
+      })
+      .catch(err => {
+        // Mockaroo, which we're using for our Mock API, only allows 200 requests per day on the free plan
+        console.log(`Sorry, buster.  No more requests allowed today!`)
+        console.error(err) // the server returned an error... probably too many requests... until we pay!
+
+        // make some backup fake data
+        const backupData = [
+          {
+            id: 1,
+            title: "Paddy heron (unidentified)",
+            country: "Brazil",
+            price: "$10.51",
+            description:
+              "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+          },
+          {
+            id: 2,
+            title: "Numbat",
+            country: "Russia",
+            price: "$2.37",
+            description:
+              "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.",
+          },
+        ]
+
+        setData(backupData)
+      })
+  }, []) // only run it once!
+  console.log(data[0].coins)
   
   return (
     <div className="header-container">
@@ -35,10 +57,10 @@ const Header = (props) => {
         <ul className="nav-links">
           {/*Coins and streak will be eventually pulled from backend*/}
           <li className="nav-item">
-            <Link to="/shop">Current Streak: 3</Link>
+            <Link to="/shop">Current Streak: {data[0].streak} </Link>
           </li>
           <li className="nav-item">
-            <Link to="/shop">Coins: 100</Link>
+            <Link to="/shop">Coins: {data[0].coins}</Link>
           </li>
           {/*This will be set to the right eventually with css*/}
           <li className="nav-item">
