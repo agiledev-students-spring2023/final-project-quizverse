6779c2586f8d96acfabc39a3d4455c4d01e9ccdc
1019jchen <jyc8938@stern.nyu.edu>
2023-03-11 19:26:48 -0500

    Daily Quiz Flashcard Update

--- front-end/src/components/Flashcard/Flashcard.js
+++ front-end/src/components/Flashcard/Flashcard.js
@@ -1,36 +1,31 @@
 import styles from './Flashcard.module.css'
 import React from "react";
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-function Flashcard() {
+function Flashcard(props) {
     return (
-        <Card sx={{ maxWidth: 500 }}>
+        <div>
+        <Card  variant = "outlined" class = "card" >
         <CardContent>
           <Typography variant='h3'  gutterBottom>
-            Abyss
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            Marine Biology Terms
+          {props.term}
           </Typography>
           <Typography variant="body1">
-            The great depths of the oceans, usually considered to be depths of 2000 to 6000 m, a region of low temperatures, high pressure, and an absence of sunlight.
+            {props.definition}
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">Flip Card</Button>
+          <Button size="small" onClick = {props.handlePrev}>Previous Card</Button>
         </CardActions>
         <CardActions>
-          <Button size="small">Previous Card</Button>
-        </CardActions>
-        <CardActions>
-          <Button size="small">Next Card</Button>
+          <Button size="small" onClick = {props.handleNext}>Next Card</Button>
         </CardActions>
         </Card>
+        </div>
     );
   }
   
