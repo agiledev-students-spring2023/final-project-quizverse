c888023a5e4f7aca461aa73d08fa845b5b1d254b
chiyanz <jonathanqyz@gmail.com>
2023-03-19 17:47:00 -0400

    updated term edittor to display dynamically

--- front-end/src/components/CreateSet/CreateSet.js
+++ front-end/src/components/CreateSet/CreateSet.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
 import {faCirclePlus} from '@fortawesome/free-solid-svg-icons'
 import styles from './CreateSet.module.css';
+import EditCard from './EditCard';
 
 const CreateSet = (props) => {
   const [title, setTitle] = useState('');
@@ -16,15 +17,43 @@ const CreateSet = (props) => {
 
   function handleChange(evt) {
     const value = evt.target.value;
-    setCards({
-      [evt.target.name]: value
-    });
-    console.log(`Field ${evt.target.name} was changed to ${value}`);
+    const id = evt.target.name;
+    const field = id.slice(0, -1);
+    const index = id.slice(id.length - 1);
+    console.log(`Field ${id} was changed to ${value} at index ${index}`);
+    const newCard = cards[index];
+    newCard[field] = value;
+    setCards(
+      cards
+        .slice(0, index)
+        .concat(newCard)
+        .concat(cards.slice(index + 1))
+    );
+    console.log(cards);
   }
 
-  const cardElements = cards.map(() => {
+  function addNew() {
+    setCards(cards.concat({ term: '', definition: '' }));
+  }
+
+  const cardElements = cards.map((info, i) => {
     return (
       <>
+        <EditCard handleChange={handleChange} index={i} term={info.term} def={info.def}></EditCard>
+      </>
+    );
+  });
+
+  return (
+    <div className={styles.SetContainer}>
+      <Container
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          overflow: 'auto',
+          overflowY: 'scroll'
+        }}>
         <FormControl sx={{ m: 1, width: '86vw' }} variant="outlined">
           <TextField
             margin="dense"
@@ -51,112 +80,14 @@ const CreateSet = (props) => {
             }}
           />
         </FormControl>
-      </>
-    );
-  });
 
-  return (
-    <div className={styles.SetContainer}>
-      <Container
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
-          overflow: 'auto',
-          overflowY: 'scroll'
-        }}>
-        <Box
-          sx={{
-            width: '80vw',
-            height: 'auto',
-            margin: 'auto',
-            borderRadius: 1,
-            backgroundColor: 'rgba(0, 7, 111, 0.4)',
-            padding: '10px'
-          }}>
-          <form onChange={handleChange}>
-            <Stack spacing={1}>
-              <TextField
-                id="filled-basic"
-                variant="standard"
-                size="small"
-                helperText="TERM"
-                name="term1"
-              />
-              <TextField
-                sx={{ padding: '5px' }}
-                id="filled-basic"
-                variant="standard"
-                size="small"
-                helperText="DEFINITION"
-                name="def1"
-              />
-            </Stack>
-          </form>
-        </Box>
+        {cardElements}
 
-        <Box
-          sx={{
-            width: '80vw',
-            height: 'auto',
-            margin: 'auto',
-            borderRadius: 1,
-            backgroundColor: 'rgba(0, 7, 111, 0.4)',
-            padding: '10px',
-            margin: '7px'
-          }}>
-          <form onChange={handleChange}>
-            <Stack spacing={1}>
-              <TextField
-                id="filled-basic"
-                variant="standard"
-                size="small"
-                helperText="TERM"
-                name="term1"
-              />
-              <TextField
-                sx={{ padding: '5px' }}
-                id="filled-basic"
-                variant="standard"
-                size="small"
-                helperText="DEFINITION"
-                name="def1"
-              />
-            </Stack>
-          </form>
-        </Box>
-
-        <Box
-          sx={{
-            width: '80vw',
-            height: 'auto',
-            margin: 'auto',
-            borderRadius: 1,
-            backgroundColor: 'rgba(0, 7, 111, 0.4)',
-            padding: '10px'
-          }}>
-          <form onChange={handleChange}>
-            <Stack spacing={1}>
-              <TextField
-                id="filled-basic"
-                variant="standard"
-                size="small"
-                helperText="TERM"
-                name="term1"
-              />
-              <TextField
-                sx={{ padding: '5px' }}
-                id="filled-basic"
-                variant="standard"
-                size="small"
-                helperText="DEFINITION"
-                name="def1"
-              />
-            </Stack>
-          </form>
-        </Box>
         <div className={styles['form-actions']}>
-          <Button variant="outlined" startIcon={<FontAwesomeIcon icon={faCirclePlus} />}>
+          <Button
+            variant="outlined"
+            onClick={addNew}
+            startIcon={<FontAwesomeIcon icon={faCirclePlus} />}>
             Add Card
           </Button>
           <Button variant="outlined">Create Set</Button>
--- front-end/src/components/CreateSet/EditCard.js
+++ front-end/src/components/CreateSet/EditCard.js
@@ -1,23 +1,38 @@
-const EditCard = ({ handleChange, name }) => {
+import { Stack, Box } from '@mui/system';
+import { TextField } from '@mui/material';
+
+const EditCard = ({ handleChange, index, term, def }) => {
   return (
-    <form onChange={handleChange}>
-      <Stack spacing={1}>
-        <TextField
-          id="filled-basic"
-          variant="standard"
-          size="small"
-          helperText="TERM"
-          name={'term' + name}
-        />
-        <TextField
-          id="filled-basic"
-          variant="standard"
-          size="small"
-          helperText="DEFINITION"
-          name={'def' + name}
-        />
-      </Stack>
-    </form>
+    <Box
+      sx={{
+        width: '80vw',
+        height: 'auto',
+        margin: '8px',
+        borderRadius: 1,
+        backgroundColor: 'rgba(0, 7, 111, 0.4)',
+        padding: '10px'
+      }}>
+      <form onChange={handleChange}>
+        <Stack spacing={1}>
+          <TextField
+            id="filled-basic"
+            variant="standard"
+            size="small"
+            helperText="TERM"
+            value={term}
+            name={'term' + index}
+          />
+          <TextField
+            id="filled-basic"
+            variant="standard"
+            size="small"
+            helperText="DEFINITION"
+            value={def}
+            name={'definition' + index}
+          />
+        </Stack>
+      </form>
+    </Box>
   );
 };
 
