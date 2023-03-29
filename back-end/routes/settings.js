6154f2b7ba24f52914e287f5014ce38119137f62
Matt-J-Dong <matthew.dong@nyu.edu>
2023-03-28 22:00:04 -0400

    modifying settings backend

--- back-end/routes/settings.js
+++ back-end/routes/settings.js
@@ -5,6 +5,40 @@ const router = express.Router();
 
 router.post('/settings', (req, res) => {
   res.send('Settings!');
+  //email and password updates at this same route or different route?
+});
+router.get('/items', (req, res) => {
+  res.send('Here are your items:');
+  const items = {
+    item1: 'Double Coins!',
+    item2: 'Triple Coins!',
+    item3: 'Streak Freeze!',
+    item4: 'Streak Protection!'
+  };
+  if (!Object.keys(items).length) {
+    console.log('no data found');
+    res.send('Whoops! You have no items.');
+  } else {
+    res.json(items);
+  }
+});
+router.get('/study-stats', (req, res) => {
+  res.send('Your study statistics:');
+  const words = {
+    mostMissed1: 'banana',
+    mostMissed2: 'canoe',
+    mostMissed3: 'The depths of the abyss'
+  };
+  if (!Object.keys(words).length) {
+    console.log('no data found');
+    res.send('Whoops! You have no study statistics. Go study to get some!');
+  } else {
+    res.json(words);
+  }
+});
+router.post('/delete', (req, res) => {
+  res.send('Your account has been deleted!');
+  //contact database and delete account information
 });
 
 module.exports = router;
