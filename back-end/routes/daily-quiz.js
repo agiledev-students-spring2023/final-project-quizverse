42a09bcef0efd8aec20801655424978ffd10ecee
Matt-J-Dong <matthew.dong@nyu.edu>
2023-03-30 22:45:20 -0400

    adding remaining back end routes

--- back-end/routes/daily-quiz.js
+++ back-end/routes/daily-quiz.js
@@ -1,5 +1,4 @@
 //Routing for all of Daily Quiz Things
-const axios = require('axios');
 const express = require('express');
 //const axios = require('axios');
 const router = express.Router();
@@ -13,21 +12,30 @@ router.get('/daily-quiz', (req, res, next) => {
     .then((apiResponse) => res.json(apiResponse.data)) // pass data along directly to client
     .catch((err) => next(err)); // pass any errors to express
 });
-  
-  // Creating a POST request for daily quiz
-  router.post('/daily-quiz', (req, res) => {
-    axios
-      .post("https://my.api.mockaroo.com/generic_post.json?key=6b3bc3e0&__method=POST")
-      .then(console.log("Succesfully sent to database"))
-      .catch(err => next(err))
-    const data = {
-      status: "Amazing success!",
-      message: "Congratulations on sending us this data!",
-      your_data: {
-      },
-    }
-    // ... then send a response of some kind to client
-    res.json(data)
-  })
 
-module.exports = router
+// Creating a POST request for daily quiz
+router.post('/daily-quiz', (req, res) => {
+  axios
+    .post('https://my.api.mockaroo.com/generic_post.json?key=6b3bc3e0&__method=POST')
+    .then(console.log('Succesfully sent to database'))
+    .catch((err) => next(err));
+  const data = {
+    status: 'Amazing success!',
+    message: 'Congratulations on sending us this data!',
+    your_data: {}
+  };
+  // ... then send a response of some kind to client
+  res.json(data);
+});
+
+router.get('/study-stats', (req, res, next) => {
+  // use axios to make a request to an API for flashcard data in the daily quiz
+  user = req.params.user;
+  //Do something with a database lookup with user. For now, this is still just calling Mockaroo.
+  axios
+    .get('https://my.api.mockaroo.com/flashcards.json?key=6b3bc3e0')
+    .then((apiResponse) => res.json(apiResponse.data)) // pass data along directly to client
+    .catch((err) => next(err)); // pass any errors to express
+});
+
+module.exports = router;
--- back-end/routes/flashcard-sets.js
+++ back-end/routes/flashcard-sets.js
@@ -21,5 +21,46 @@ router.get('/search/:searchTerm', (req, res) => {
     })
     .catch((err) => next(err));
 });
-
+router.get('/flashcard-set/:id', (req, res) => {
+  // Ideally, this will be replaced later with flashcard sets (via the backend), as opposed to just
+  // the flashcards themselves.
+  axios
+    .get(`https://my.api.mockaroo.com/flashcards.json?key=6b3bc3e0`)
+    .then((apiResponse) => {
+      const data = apiResponse.data;
+      const filteredData = data.filter((item) => {
+        return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+      res.json(filteredData);
+    })
+    .catch((err) => next(err));
+});
+router.get('/flashcards', (req, res) => {
+  // Ideally, this will be replaced later with flashcard sets (via the backend), as opposed to just
+  // the flashcards themselves.
+  axios
+    .get(`https://my.api.mockaroo.com/flashcards.json?key=6b3bc3e0`)
+    .then((apiResponse) => {
+      const data = apiResponse.data;
+      const filteredData = data.filter((item) => {
+        return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+      res.json(filteredData);
+      /*
+      This data would be something like this:
+          {
+            numCards: 80,
+            title: 'Realigned background approach',
+            description:
+              'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit.'
+          },
+          {
+            numCards: 92,
+            title: 'Robust responsive success',
+            description: 'Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.'
+          }
+      */
+    })
+    .catch((err) => next(err));
+});
 module.exports = router;
