ebcab8bc76e301e1a6560661e4110563e4215876
Matt-J-Dong <matthew.dong@nyu.edu>
2023-03-30 22:15:37 -0400

    comments

--- back-end/routes/daily-quiz.js
+++ back-end/routes/daily-quiz.js
@@ -1,17 +1,17 @@
 //Routing for all of Daily Quiz Things
 const express = require('express');
-const axios = require('axios')
-const router = express.Router()
+const axios = require('axios');
+const router = express.Router();
 
-router.get("/daily-quiz", async (req, res, next) => {
-    // use axios to make a request to an API for flashcard data in the daily quiz
-    user = req.params.user
-    //Do something with a database lookup with user. For now, this is still just calling Mockaroo.
-    axios
-      .get("https://my.api.mockaroo.com/flashcards.json?key=6b3bc3e0")
-      .then(apiResponse => res.json(apiResponse.data)) // pass data along directly to client
-      .catch(err => next(err)) // pass any errors to express
-  })
+router.get('/daily-quiz', (req, res, next) => {
+  // use axios to make a request to an API for flashcard data in the daily quiz
+  user = req.params.user;
+  //Do something with a database lookup with user. For now, this is still just calling Mockaroo.
+  axios
+    .get('https://my.api.mockaroo.com/flashcards.json?key=6b3bc3e0')
+    .then((apiResponse) => res.json(apiResponse.data)) // pass data along directly to client
+    .catch((err) => next(err)); // pass any errors to express
+});
   
   // Creating a POST request for daily quiz
   router.post('/daily-quiz', (req, res) => {
