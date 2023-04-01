5393851b0afa966684b26398df578b9e8cbbbc8d
mertbayt <mib335@nyu.edu>
2023-04-01 15:52:12 -0400

    routed and setup login and signup system between back and front end

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,60 +1,63 @@
-import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState, useContext} from 'react';
+import {useNavigate} from 'react-router-dom';
 import './Login.css';
-import { UserContext } from '../Landing/UserContext';
+import {UserContext} from '../Landing/UserContext';
+import axios from 'axios';
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
-  const { userCredentials } = useContext(UserContext);
+    const [username,
+        setUsername] = useState('');
+    const [password,
+        setPassword] = useState('');
+    const navigate = useNavigate();
+    const {userCredentials} = useContext(UserContext);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const foundUser = userCredentials.find(
-      (user) => user.username === username && user.password === password
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:3001/login', {username, password});
+            alert(response.data.message);
+            navigate('/home');
+        } catch (error) {
+            alert(error.response
+                ?.data
+                    ?.message || 'An error occurred');
+        }
+    };
+
+    return (
+        <div className="login-page-container">
+            <h1 className="login-page-title">Log In</h1>
+            <form className="login-page-form" onSubmit={handleSubmit}>
+                <div className="login-page-input-container">
+                    <label htmlFor="username" className="login-page-label">
+                        Username:
+                    </label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        className="login-page-input"/>
+                </div>
+                <div className="login-page-input-container">
+                    <label htmlFor="password" className="login-page-label">
+                        Password:
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        className="login-page-input"/>
+                </div>
+                <button type="submit" className="login-page-button">
+                    Log In
+                </button>
+            </form>
+        </div>
     );
-    if (foundUser) {
-      navigate('/home');
-    } else {
-      alert('Invalid username or password');
-    }
-  };
-
-  return (
-    <div className="login-page-container">
-      <h1 className="login-page-title">Log In</h1>
-      <form className="login-page-form" onSubmit={handleSubmit}>
-        <div className="login-page-input-container">
-          <label htmlFor="username" className="login-page-label">
-            Username:
-          </label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            className="login-page-input"
-          />
-        </div>
-        <div className="login-page-input-container">
-          <label htmlFor="password" className="login-page-label">
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            className="login-page-input"
-          />
-        </div>
-        <button type="submit" className="login-page-button">
-          Log In
-        </button>
-      </form>
-    </div>
-  );
 }
 
 export default LoginPage;
