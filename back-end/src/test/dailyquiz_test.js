8b8b583da3feeee4cacfc3534b86469317c9e4da
1019jchen <jyc8938@stern.nyu.edu>
2023-04-03 17:38:58 -0400

    commit before i delete things to test

--- back-end/src/test/dailyquiz_test.js
+++ back-end/src/test/dailyquiz_test.js
@@ -3,29 +3,24 @@ const assert = require('assert');
 const auth = require('../../routes/auth');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-
+let should = chai.should();
 chai.use(chaiHttp);
 
 describe('Daily Quiz', function () {
     // one particular unit test
-    describe('Login Route', function () {
+    describe('Get the Daily Quiz flashcards', function () {
       // assert what should be returned
-      it('Login', function () {
+      it('it should GET the vocab and definitions', (done)=> {
         var host = 'http://localhost:3001';
-        var path = '/login';
+        var path = '/daily-quiz';
         // test that assertion
         chai
           .request(host)
-          .post(path)
-          // .field('myparam' , 'test')
-          .set('content-type', 'application/x-www-form-urlencoded')
-          .send({ myparam: 'test' })
-          .end(function (error, response, body) {
-            if (error) {
-              console.log('BIG ERROR');
-            } else {
-              console.log('YAHOO');
-            }
+          .get(path)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            done();
           });
       });
     });
