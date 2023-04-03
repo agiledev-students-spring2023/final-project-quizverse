a355d1c1e34feb7e0fd4c314e7e08b77a5a59490
Matt-J-Dong <matthew.dong@nyu.edu>
2023-04-03 12:55:53 -0400

    couple more unit tests

--- back-end/src/test/auth_test.js
+++ back-end/src/test/auth_test.js
@@ -9,11 +9,53 @@ chai.use(chaiHttp);
 // a set of tests of array functions
 describe('Auth', function () {
   // one particular unit test
-  describe('someMethod()', function () {
+  describe('Login Route', function () {
     // assert what should be returned
-    it('sussy', function () {
+    it('Login', function () {
       var host = 'http://localhost:3001';
       var path = '/login';
+      // test that assertion
+      chai
+        .request(host)
+        .post(path)
+        // .field('myparam' , 'test')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({ myparam: 'test' })
+        .end(function (error, response, body) {
+          if (error) {
+            console.log('BIG ERROR');
+          } else {
+            console.log('YAHOO');
+          }
+        });
+    });
+  });
+  describe('Register Route', function () {
+    // assert what should be returned
+    it('Register', function () {
+      var host = 'http://localhost:3001';
+      var path = '/register';
+      // test that assertion
+      chai
+        .request(host)
+        .post(path)
+        // .field('myparam' , 'test')
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({ myparam: 'test' })
+        .end(function (error, response, body) {
+          if (error) {
+            console.log('BIG ERROR');
+          } else {
+            console.log('YAHOO');
+          }
+        });
+    });
+  });
+  describe('Logout Route', function () {
+    // assert what should be returned
+    it('Logout', function () {
+      var host = 'http://localhost:3001';
+      var path = '/logout';
       // test that assertion
       chai
         .request(host)
