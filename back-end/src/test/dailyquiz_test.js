1d51891a48206bcdbf2e6e4fa2cc179d742ccd40
1019jchen <jyc8938@stern.nyu.edu>
2023-04-05 10:51:57 -0400

    synced with master

--- back-end/src/test/dailyquiz_test.js
+++ back-end/src/test/dailyquiz_test.js
@@ -24,47 +24,6 @@ describe('Daily Quiz', function () {
           });
       });
     });
-    describe('Register Route', function () {
-      // assert what should be returned
-      it('Register', function () {
-        var host = 'http://localhost:3001';
-        var path = '/register';
-        // test that assertion
-        chai
-          .request(host)
-          .post(path)
-          // .field('myparam' , 'test')
-          .set('content-type', 'application/x-www-form-urlencoded')
-          .send({ myparam: 'test' })
-          .end(function (error, response, body) {
-            if (error) {
-              console.log('BIG ERROR');
-            } else {
-              console.log('YAHOO');
-            }
-          });
-      });
-    });
-    describe('Logout Route', function () {
-      // assert what should be returned
-      it('Logout', function () {
-        var host = 'http://localhost:3001';
-        var path = '/logout';
-        // test that assertion
-        chai
-          .request(host)
-          .post(path)
-          // .field('myparam' , 'test')
-          .set('content-type', 'application/x-www-form-urlencoded')
-          .send({ myparam: 'test' })
-          .end(function (error, response, body) {
-            if (error) {
-              console.log('BIG ERROR');
-            } else {
-              console.log('YAHOO');
-            }
-          });
-      });
-    });
-  });
+    
+})
   
