13f48b97221cd0a7beffc07d0c29004ba933a9a7
chiyanz <jonathanqyz@gmail.com>
2023-04-06 16:43:08 -0400

    fixed hamburger menu interaction and added new links

--- front-end/src/components/Layout/Header/Menu.styled.js
+++ front-end/src/components/Layout/Header/Menu.styled.js
@@ -4,7 +4,7 @@ export const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background: ${({ theme }) => theme.primaryLight};
+  background-color: skyblue;
   height: 100vh;
   text-align: left;
   padding: 2rem;
@@ -12,21 +12,21 @@ export const StyledMenu = styled.nav`
   top: 0;
   left: 0;
   transition: transform 0.3s ease-in-out;
-  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
   @media (max-width: ${({ theme }) => theme.mobile}) {
     width: 100%;
   }
 
   a {
-    font-size: 2rem;
+    font-size: 1.5rem;
     text-transform: uppercase;
     padding: 2rem 0;
     font-weight: bold;
     letter-spacing: 0.5rem;
-    color: ${({ theme }) => theme.primaryDark};
+    color: #000;
     text-decoration: none;
     transition: color 0.3s linear;
-    
+
     @media (max-width: ${({ theme }) => theme.mobile}) {
       font-size: 1.5rem;
       text-align: center;
@@ -35,6 +35,5 @@ export const StyledMenu = styled.nav`
     &:hover {
       color: ${({ theme }) => theme.primaryHover};
     }
-    
   }
 `;
