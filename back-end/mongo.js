aeef462005e0d146a6127fce563947cac901fad5
RohanK6 <rohan.khanderia@gmail.com>
2023-04-12 09:02:50 -0400

    Re-write User creation samples

--- back-end/mongo.js
+++ back-end/mongo.js
@@ -8,40 +8,30 @@ const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_DB = process.env.MONGO_DB;
 
-main().catch((err) => console.log(err));
 // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 async function main() {
   await mongoose.connect(
     `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_DB}.zkgvem0.mongodb.net/?retryWrites=true&w=majority`
   );
-  console.log('YOYLECAKE');
-  const kittySchema = new mongoose.Schema({
-    name: String
+  const primaryUser = new User({
+    username: 'primary',
+    password: 'primary!@#'
   });
-  // NOTE: methods must be added to the schema before compiling it with mongoose.model()
-  kittySchema.methods.speak = function speak() {
-    const greeting = this.name ? 'Meow name is ' + this.name : "I don't have a name";
-    console.log(greeting);
-  };
-  const Kitten = mongoose.model('Kitten', kittySchema);
-  const silence = new Kitten({ name: 'Silence' });
-  console.log(silence.name); // 'Silence'
-  const fluffy = new Kitten({ name: 'fluffy' });
-  fluffy.speak(); // "Meow name is fluffy"
-  await fluffy.save();
-  fluffy.speak();
-  const kittens = await Kitten.find();
-  console.log(kittens);
-  await Kitten.find({ name: /^fluff/ });
-  const user1 = new User({ username: 'Bob', password: 'default!password' });
-  await user1.save();
-  try {
-    const user2 = await User.create({
-      username: 'Sally',
-      password: 'quizVerse!'
-    });
-    console.log(user2);
-  } catch (e) {
-    console.log(e.message);
-  }
+  await primaryUser.save();
+
+  const secondaryUser = new User({
+    username: 'secondary',
+    password: 'secondary!@#'
+  });
+  await secondaryUser.save();
+
+  console.log('All Users: ', await User.find({}));
+
+  console.log(
+    "Finding a user with username 'primary': ",
+    await User.findOne({ username: 'primary' })
+  );
+
 }
+
+main();
