0b9f58c46971811e79e487583642025d15568be4
RohanK6 <rohan.khanderia@gmail.com>
2023-04-19 22:27:45 +0100

    Get mongo data for specific flashcard set

--- back-end/routes/flashcard-sets.js
+++ back-end/routes/flashcard-sets.js
@@ -61,45 +61,20 @@ router.get('/flashcard-sets', (req, res) => {
     });
 });
 
-
 router.get('/flashcard-set/:id', (req, res) => {
   const id = req.params.id;
   if (!id) {
     res.status(400).send({ message: 'missing set id' });
   }
 
-  axios
-    .get(`https://my.api.mockaroo.com/flashcards.json?key=6b3bc3e0`)
-    .then((apiResponse) => {
-      const data = apiResponse.data;
-
-      const backupData = {
-        id,
-        title: 'Agile Quiz',
-        description:
-          'This set is a mock quiz set that will be used to test dynamic study set editting. The id of the set will be the id passed in via the query params',
-        cards: [
-          { term: 'card1', definition: 'card1 def' },
-          { term: 'card2', definition: 'card2 def' },
-          { term: 'card3', definition: 'card3 def' }
-        ]
-      };
-      res.status(200).json(backupData);
+  // find flashcard set by object id
+  FlashcardSet.findById(id)
+    .then((setFromMongo) => {
+      console.log(setFromMongo);
+      res.json(setFromMongo);
     })
     .catch((err) => {
-      const backupData = {
-        id,
-        title: 'Agile Quiz',
-        description:
-          'This set is a mock quiz set that will be used to test dynamic study set editting. The id of the set will be the id passed in via the query params',
-        cards: [
-          { term: 'card1', definition: 'card1 def' },
-          { term: 'card2', definition: 'card2 def' },
-          { term: 'card3', definition: 'card3 def' }
-        ]
-      };
-      res.json(backupData)
-      res.status(500).send({ message: 'api request error' });
+      console.error(err);
     });
 });
 router.get('/flashcards', (req, res) => {
