1f979021a73726c968a9572a7dd31f10b8d5d05b
Matt-J-Dong <matthew.dong@nyu.edu>
2023-04-20 14:41:00 -0400

    haskdj

--- front-end/src/components/Login/Login.js
+++ front-end/src/components/Login/Login.js
@@ -1,74 +1,71 @@
-import React, {useState, useContext} from 'react';
-import {useEffect } from "react"
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+//import useContext from 'react';
+//import {useEffect } from "react"
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
-import {UserContext} from '../Landing/UserContext';
+//import {UserContext} from '../Landing/UserContext';
 import axios from 'axios';
 
-
-
 function LoginPage() {
-    const [username,
-        setUsername] = useState('');
-    const [password,
-        setPassword] = useState('');
-    const navigate = useNavigate();
-    const {userCredentials} = useContext(UserContext);
-    
-    const [cookieData, setCookieData] = useState({})
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  //const {userCredentials} = useContext(UserContext);
 
-    const handleSubmit = async(event) => {
-        event.preventDefault();
+  //const [cookieData, setCookieData] = useState({})
 
-        try {
-          const response = await axios.post('http://localhost:3001/login', { username, password });
-          // if (response.data && response.data.token){
-          //   //alert(response.data.token);
-          //   //console.log(response.data.token);
-          //   //localStorage.setItem("token", response.data.token);
-          //   //future requests should have
-          // }
-          alert(response.data.message);
-          navigate('/home');
-        } catch (error) {
-            alert(error.response
-                ?.data
-                    ?.message || 'An error occurred on login');
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div className="login-page-container">
-            <h1 className="login-page-title">Log In</h1>
-            <form className="login-page-form" onSubmit={handleSubmit}>
-                <div className="login-page-input-container">
-                    <label htmlFor="username" className="login-page-label">
-                        Username:
-                    </label>
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(event) => setUsername(event.target.value)}
-                        className="login-page-input"/>
-                </div>
-                <div className="login-page-input-container">
-                    <label htmlFor="password" className="login-page-label">
-                        Password:
-                    </label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
-                        className="login-page-input"/>
-                </div>
-                <button type="submit" className="login-page-button">
-                    Log In
-                </button>
-            </form>
+    try {
+      const response = await axios.post('http://localhost:3001/login', { username, password });
+      // if (response.data && response.data.token){
+      //   //alert(response.data.token);
+      //   //console.log(response.data.token);
+      //   //localStorage.setItem("token", response.data.token);
+      //   //future requests should have
+      // }
+      alert(response.data.message);
+      navigate('/home');
+    } catch (error) {
+      alert(error.response?.data?.message || 'An error occurred on login');
+    }
+  };
+
+  return (
+    <div className="login-page-container">
+      <h1 className="login-page-title">Log In</h1>
+      <form className="login-page-form" onSubmit={handleSubmit}>
+        <div className="login-page-input-container">
+          <label htmlFor="username" className="login-page-label">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            className="login-page-input"
+          />
         </div>
-    );
+        <div className="login-page-input-container">
+          <label htmlFor="password" className="login-page-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="login-page-input"
+          />
+        </div>
+        <button type="submit" className="login-page-button">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
