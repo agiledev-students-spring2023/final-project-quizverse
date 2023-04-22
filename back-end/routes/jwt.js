16a43c428f61626af4fbb7e085b3c1b6a09702e6
1019jchen <jyc8938@stern.nyu.edu>
2023-04-22 12:49:05 -0400

    uncommenting

--- back-end/routes/jwt.js
+++ back-end/routes/jwt.js
@@ -8,12 +8,12 @@ const verifyToken = (req, res, next) => {
   if (!token) {
     return res.status(403).send('A token is required for authentication');
   }
-  // try {
-  //   const decoded = jwt.verify(token, config.JWT_SECRET);
-  //   req.user = decoded;
-  // } catch (err) {
-  //   return res.status(401).send('Invalid Token');
-  // }
+  try {
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send('Invalid Token');
+  }
   return next();
 };
 
