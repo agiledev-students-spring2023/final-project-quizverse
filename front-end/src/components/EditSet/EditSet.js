887710b23aa287df6d3cfdb0bff1be405f110f26
1019jchen <jyc8938@stern.nyu.edu>
2023-04-30 14:14:33 -0400

    added way to delete a set

--- front-end/src/components/EditSet/EditSet.js
+++ front-end/src/components/EditSet/EditSet.js
@@ -138,6 +138,9 @@ const EditSet = (props) => {
           <Button onClick={handleSubmit} disabled={!editted} variant="outlined">
             Save
           </Button>
+          <Button>
+            Hello
+          </Button>
         </div>
         <FormControl sx={{ m: 1, width: '86vw' }} variant="outlined">
           <TextField
